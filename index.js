639c78b5ae60aac822cf4af985dec3d3fef2a9ae
Thomas Jolley <tomjolley81@gmail.com>
2018-11-07 18:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  kitten = [...kittens, name]
+  return kitten
+}
+function prependKitten(name) {
+  kitten = [name, ...kittens]
+  return kitten
+}
+function removeLastKitten() {
+  kitten = kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten() {
+  kitten = kittens.slice(1,-1)
+}
